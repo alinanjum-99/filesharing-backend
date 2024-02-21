d31b08f965999af17e1c21f9766d5cab98b922ca
alinanjum-99 <alinanjum9@gmail.com>
2024-02-21 17:26:47 -0600

    updating cors on server.js
    cors func had a missing var. updated the missing var to see if it runs on the deployed server

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const path = require('path');
 const cors = require('cors');
 app.use(express.static('public'));
 app.use(express.json());
 
 //cors 
-const corsOptions = {
-    origin: process.env.Allowed_Clients
 
+
+
+const corsOptions ={
+   origin:process.env.Allowed_Clients.split(','), 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
 }
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 const connectDB = require('./config/db');
 connectDB();
 app.set('views', path.join(__dirname, '/views'));
@@ -22,9 +28,9 @@ app.set('view engine', 'ejs');
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
-app.use(cors(corsOptions));
 
 
 
-app.listen(PORT, console.log(`Listening on port ${PORT}.`));
+
+app.listen(process.env.PORT, console.log(`Listening on port ${PORT}.`));
 
